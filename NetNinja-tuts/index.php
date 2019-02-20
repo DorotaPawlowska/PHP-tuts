b07540b3aba872224b945819fd3210db04a81db4
Dorota Pawlowska <dpawlowska@op.pl>
2019-02-20 21:09:09 +0100

    PHP Tutorial (& MySQL) #15 - Include & Require
    Changes to be committed:
    	new file:   content.php
    	modified:   index.php
    	new file:   ninjas.php

--- NetNinja-tuts/index.php
+++ NetNinja-tuts/index.php
@@ -1,45 +1,14 @@
 <?php 
 
-// variable scope
+    // include('ninjas.php');
+    // require('ninjas.php');
+ 
+    // include('ninjass.php');
+    // require('ninjass.php');
+    require 'ninjas.php';
+    include 'ninjas.php';
 
-// local variables
-function myFunc(){
-    $price = 10;
-    echo $price;
-}
-
-// myFunc();
-// echo $price;
-
-
-function myFuncTwo($age){
-    echo $age;
-}
-
-// myFuncTwo(25);
-// echo $age;
-
-// global variables
-
-$name = 'mario';
-
-// function sayHello(){
-//     global $name;
-//     $name = 'youshi';
-//     echo "hello $name";
-// }
-
-// sayHello();
-// echo $name;
-
-function sayBye(&$name){ // referencja
-    $name = "wario";
-    echo "bye $name";
-}
-
-sayBye($name);
-echo $name;
-
+    echo 'end of php';
 
 ?>
 
@@ -52,6 +21,10 @@ echo $name;
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
+
+    <?php  include('content.php'); ?>
+    <?php  include('content.php'); ?>
+    <?php  include('content.php'); ?>
 
 </body>
 </html>
