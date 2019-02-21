cf462761a5d4da79642f4eec374fbc1db4b5e1dc
Dorota Pawlowska <dpawlowska@op.pl>
2019-02-21 21:27:30 +0100

    PHP Tutorial (& MySQL) #17 - Forms in PHP
    Changes to be committed:
    	new file:   add.php
    	deleted:    styles/main.css
    	modified:   templates/header.php

--- NetNinja-tuts/templates/header.php
+++ NetNinja-tuts/templates/header.php
@@ -3,10 +3,26 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Ninja Pizza</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <!-- Compiled and minified CSS -->
     <link rel="stylesheet" 
     href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <link rel="stylesheet" type="text/css" media="screen" href="styles/main.css">
+
+    <style type="text/css">
+        .brand-text{
+            color: #cbb09c !important;
+        }
+        .brand{
+            background: #cbb09c !important;
+        }
+        form{
+            max-width: 460px;
+            margin: 20px auto;
+            padding: 20px;
+        }
+    </style>
+    
 </head>
 <body class="grey lighten-4">
 <nav class="white z-depth-0">
