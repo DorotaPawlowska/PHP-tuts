4d212945fcdcb6f3bbf0a6394bf00188d85038aa
Dorota Pawlowska <dpawlowska@op.pl>
2019-02-23 20:22:34 +0100

    PHP Tutorial (& MySQL) #21 - Showing Errors
    Changes to be committed:
    	modified:   add.php

--- NetNinja-tuts/add.php
+++ NetNinja-tuts/add.php
@@ -1,40 +1,37 @@
 <?php 
 
-    // if(isset($_GET['submit'])){
-    //     echo $_GET['email'];
-    //     echo $_GET['title'];
-    //     echo $_GET['ingredients'];
-    // }
+    $title = $email = $ingredients = '';
+    $errors = array('email'=>'', 'title'=>'','ingredients'=>'');
 
     if(isset($_POST['submit'])){
 
         //check email
         if(empty($_POST['email'])){
-            echo 'an email is required <br />';
+            $errors['email'] = 'an email is required <br />';
         }else{
             $email = $_POST['email'];
             if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-                echo "email must be a valid email address";
+                $errors['email'] = "email must be a valid email address";
             }
         }
 
         //check title
         if(empty($_POST['title'])){
-            echo 'an title is required <br />';
+            $errors['title'] = 'an title is required <br />';
         }else{
             $title = $_POST['title'];
             if(!preg_match( '/^[a-zA-Z\s]+$/', $title)){
-                echo 'title must be letters and spaces only';
+                $errors['title'] = 'title must be letters and spaces only';
             }
         }
 
         //check list of ingre
         if(empty($_POST['ingredients'])){
-            echo 'an ingredients is required <br />';
+            $errors['ingredients'] = 'an ingredients is required <br />';
         }else{
             $ingredients = $_POST['ingredients'];
             if(!preg_match( '/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)){
-                echo 'ingredients must be a comma separated list';
+                $errors['ingredients'] = 'ingredients must be a comma separated list';
             }
         }
     } // end of the post check
@@ -49,15 +46,23 @@
     <section class="container grey-text">
         <h4 class="center">Add a Pizza</h4>
         <form action="add.php" method="POST" class="white">
+
             <label for="">Your Email:</label>
-            <input type="text" name="email">
+            <input type="text" name="email" value="<?php echo htmlspecialchars($email) ?>">
+            <div class="red-text"><?php echo $errors['email']; ?></div>
+
             <label for="">Pizza title:</label>
-            <input type="text" name="title">
+            <input type="text" name="title" value="<?php echo htmlspecialchars($title) ?>">
+            <div class="red-text"><?php echo $errors['title']; ?></div>
+
             <label for="">Ingredients (comma separated):</label>
-            <input type="text" name="ingredients">
+            <input type="text" name="ingredients" value="<?php echo htmlspecialchars($ingredients) ?>">
+            <div class="red-text"><?php echo $errors['ingredients']; ?></div>
+
             <div class="center">
                 <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
             </div>
+
         </form>
     </section>
 
