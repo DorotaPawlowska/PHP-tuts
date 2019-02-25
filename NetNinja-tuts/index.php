1a6b378ebc72ee0c319359370262aecb052bc5df
Dorota Pawlowska <dpawlowska@op.pl>
2019-02-25 21:03:17 +0100

    PHP Tutorial (& MySQL) #26 - Getting Data From a Database
    Changes to be committed:
    	modified:   index.php

--- NetNinja-tuts/index.php
+++ NetNinja-tuts/index.php
@@ -8,6 +8,23 @@
         echo 'Connection error' . mysqli_connect_error();
     }
 
+    // write query for all pizzas
+    $sql = 'SELECT title, ingredients, id FROM pizzas';
+
+    //make query and get results
+    $result = mysqli_query($conn, $sql);
+
+    //fetch the resulting rows as an array
+    $pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+    // good practice for cleaning after connection to DB
+    // free result from memory
+    mysqli_free_result($result);
+    //close connection
+    mysqli_close($conn);
+
+
+    print_r($pizzas);
 
 ?>
 
