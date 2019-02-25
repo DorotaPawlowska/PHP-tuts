2ea8155a43630bd66fa5c42537a343ff4997dadc
Dorota Pawlowska <dpawlowska@op.pl>
2019-02-25 20:53:07 +0100

    Changes to be committed: 	modified:   index.php

--- NetNinja-tuts/index.php
+++ NetNinja-tuts/index.php
@@ -1,5 +1,14 @@
 <?php 
 
+    //MySQLi or PDO - connect to DB
+    $conn = mysqli_connect('localhost', 'shaun', 'qwerty', 'ninja_pizza');
+
+    //check the connection
+    if(!$conn){
+        echo 'Connection error' . mysqli_connect_error();
+    }
+
+
 ?>
 
 <!DOCTYPE html>
