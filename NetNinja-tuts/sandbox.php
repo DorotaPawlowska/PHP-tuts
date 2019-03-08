75d05066b10ce63d3f7ca1468ba2d2839296a439
dorota <dpawlowska@op.pl>
2019-03-08 21:05:05 +0100

    PHP Tutorial (& MySQL) #35 - Superglobals
    Changes to be committed:
    	modified:   NetNinja-tuts/add.php
    	modified:   NetNinja-tuts/sandbox.php

--- NetNinja-tuts/sandbox.php
+++ NetNinja-tuts/sandbox.php
@@ -1,17 +1,14 @@
 <?php 
 
-//ternary operators
+//superglobal
+// $_GET['name'], $_POST['name']
 
-$score = 20;
+echo $_SERVER['SERVER_NAME'] . '<br />';
+echo $_SERVER['REQUEST_METHOD'] . '<br />';
+echo $_SERVER['SCRIPT_FILENAME'] . '<br />';
+echo $_SERVER['PHP_SELF'] . '<br />';
 
-// if($score > 40){
-//     echo 'high score!';
-// }else{
-//     echo 'lowe score :(';
-// }
-
-// $val = $score > 40 ? 'high score!' : 'lowe score :(';
-// echo $val;
+// $_SESSIO, $_COOKIE
 
 ?>
 
@@ -27,7 +24,6 @@ $score = 20;
 </head>
 <body>
     
-<p><?php echo $score > 40 ? 'high score!' : 'lowe score :('; ?></p>
 
 </body>
 </html>
