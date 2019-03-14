d2e65f9f63a524fec9c7ca8c8eaf4e492e81ea73
dorota <dpawlowska@op.pl>
2019-03-14 20:16:27 +0100

    PHP Tutorial (& MySQL) #42 - Classes & Objects (part 2)
    Changes to be committed:
    	modified:   NetNinja-tuts/PHPTutorialForBeginners/sandbox4.php

--- NetNinja-tuts/PHPTutorialForBeginners/sandbox4.php
+++ NetNinja-tuts/PHPTutorialForBeginners/sandbox4.php
@@ -3,8 +3,8 @@
     // classes
 
     class User {
-        public $email;
-        public $name;
+        private $email;
+        private $name;
 
         public function __construct($name, $email){
             // $this->email = 'mario@thenetninja.com';
@@ -17,12 +17,46 @@
             // echo 'the user logged in';
             echo $this->name . ' logged in';
         }
+
+        public function getName(){
+            return $this->name;    
+        }
+
+        public function setName($name){
+            if(is_string($name) && strlen($name) > 1){
+                $this->name = $name;
+                return "name has been updated to $name ";
+            }else{
+                return "not a valid name ";
+            }
+        }
     }
 
     $userTwo = new User('yoshi', 'yoshi@dot.pl');
-    $userTwo->login();
-    echo '<br>' . $userTwo->name;
-    echo '<br>' . $userTwo->email;
+    // $userTwo->name = 'mario';
+    // echo '<br>' . $userTwo->name;
+    // echo '<br>' . $userTwo->getName();
+    // echo '<br>' . $userTwo->setName(50);
+    // echo '<br>' . $userTwo->setName('shaun');
+    echo '<br>' . $userTwo->getName();
+    // echo '<br>' . $userTwo->name; // nie można teraz
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+    // $userTwo->login();
+    // echo '<br>' . $userTwo->email;
 
     // $userOne = new User();
     // $userOne->login();
