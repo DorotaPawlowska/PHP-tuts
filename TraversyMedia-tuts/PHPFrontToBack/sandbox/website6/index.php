380878be2ace9da58bb5475ded226bfd69b7354c
dorota <dpawlowska@op.pl>
2019-03-26 22:05:21 +0100

    PHP Front To Back [Part 18] - PHP & AJAX
    Changes to be committed:
    	new file:   TraversyMedia-tuts/PHPFrontToBack/sandbox/website6/index.php
    	new file:   TraversyMedia-tuts/PHPFrontToBack/sandbox/website6/suggest.php

---
+++ TraversyMedia-tuts/PHPFrontToBack/sandbox/website6/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link 
+    href="https://stackpath.bootstrapcdn.com/bootswatch/4.3.1/sketchy/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-N8DsABZCqc1XWbg/bAlIDk7AS/yNzT5fcKzg/TwfmTuUqZhGquVmpb5VvfmLcMzp" crossorigin="anonymous">    
+</head>
+<body>
+    <div class="container">
+        <h1>Search Users</h1>
+        <form action="">
+            Search Users: <input type="text" class="form-control" onkeyup="showSuggestion(this.value)">
+        </form>
+        <p>Suggestions: <span id="output" stylr="font-weight: bold;"></span></p>
+    </div>
+
+    <script>
+        function showSuggestion(str) {
+            // console.log(str);
+            if(str.length == 0){
+                document.getElementById('output').innerHTML = '';
+            } else {
+                // ajax
+                const xmlhttp = new XMLHttpRequest();
+                xmlhttp.onreadystatechange = function () {
+                    if(this.readyState == 4 && this.status == 200){
+                        document.getElementById('output').innerHTML = this.responseText;
+                    }
+                }
+                xmlhttp.open("GET", "suggest.php?q="+str, true);
+                xmlhttp.send();
+            }
+        }
+    </script>
+
+</body>
+</html>
