ba8d752724d4b0b18f491b84446f4f075d0c7e3f
dorota <dpawlowska@op.pl>
2020-03-06 22:42:02 +0100

    Laravel 6 Tutorial for Beginners #18 - Creating a Web Form
    Changes to be committed:
    	modified:   NetNinja-tuts/Laravel6TutorialForBeginners/pizzahouse/resources/views/pizzas/create.blade.php
    	modified:   NetNinja-tuts/Laravel6TutorialForBeginners/pizzahouse/resources/views/welcome.blade.php

--- NetNinja-tuts/Laravel6TutorialForBeginners/pizzahouse/resources/views/pizzas/create.blade.php
+++ NetNinja-tuts/Laravel6TutorialForBeginners/pizzahouse/resources/views/pizzas/create.blade.php
@@ -1,12 +1,26 @@
 @extends('layouts.layout')
 
 @section('content')
-<div class="flex-center position-ref full-height">
-      
-  <div class="content">
-    <div class="title m-b-md">
-      add a new pizza
-    </div>
+  <div class="wrapper create-pizza">
+    <h1>create a new pizza</h1>
+    <form action="" method="">
+      <label for="name">your name: </label>
+      <input type="text" name="name" id="name">
+      <label for="type">choose pizza type: </label>
+      <select name="type" id="type">
+      <option value="margarita">Margarita</option>
+      <option value="hawaiian">Hawaiian</option>
+      <option value="veg supreme">Veg Supreme</option>
+      <option value="volcano">Volcano</option>
+      </select>
+      <label for="base">choose pizza base: </label>
+      <select name="base" id="base">
+      <option value="cheesy crust">Cheesy Crust</option>
+      <option value="garlic crust">Garlic Crust</option>
+      <option value="thin & crispy">Thin & Crispy</option>
+      <option value="thick">Thick</option>
+      </select>
+      <input type="submit" value="Order Pizza">
+    </form>
   </div>
-</div>
 @endsection
